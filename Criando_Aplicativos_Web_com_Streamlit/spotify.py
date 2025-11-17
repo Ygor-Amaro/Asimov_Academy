ba253a03b448f4687a05cdbb064fc05c8f4a1b93
Ygor Amaro <ykba09@gmail.com>
2025-11-16 23:54:27 -0300

    streamlit funcionando

--- Criando_Aplicativos_Web_com_Streamlit/spotify.py
+++ Criando_Aplicativos_Web_com_Streamlit/spotify.py
@@ -3,5 +3,12 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('Criando_Aplicativos_Web_com_Streamlit\01 Spotify.csv')
-df[df["Streams"] > 1000000000] #Filtra as músicas com mais de 1 bilhão de streams
+df = pd.read_csv(
+    r'C:/Users/ykba0/OneDrive/Documentos/ASIMOV_ACADEMY/Criando_Aplicativos_Web_com_Streamlit/DataSpotify.csv', 
+    engine='python',
+    encoding='utf-8',
+    sep=','
+)
+
+print(df.columns)
+df[df["Stream"] > 1000000000] #Filtra as músicas com mais de 1 bilhão de streams

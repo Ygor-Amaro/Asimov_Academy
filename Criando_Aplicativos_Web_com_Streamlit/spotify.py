af3fe439b45756e0669474b295bb04a525ac3aaa
Ygor Amaro <ykba09@gmail.com>
2025-11-17 23:31:01 -0300

    layout atualizado

--- Criando_Aplicativos_Web_com_Streamlit/spotify.py
+++ Criando_Aplicativos_Web_com_Streamlit/spotify.py
@@ -18,15 +18,15 @@ print(df.columns)
 df.set_index("Track", inplace=True)
 
 artists = df["Artist"].value_counts().index
-artist = st.selectbox("Artista", artists)
+artist = st.sidebar.selectbox("Artista", artists)
 df_filtered = df[df["Artist"] == artist]
 
 albuns = df_filtered["Album"].value_counts().index
-album = st.selectbox("Album", albuns)
+album = st.sidebar.selectbox("Album", albuns)
 df_filtered2 = df[df["Album"] == album]
 
-display = st.checkbox("Mostrar Dados")
-if display:
-    st.bar_chart(df_filtered2["Stream"])
+col1, col2 = st.columns(2)
 
+col1.bar_chart(df_filtered2["Stream"])
+col2.bar_chart(df_filtered2["most_playedon"])
 st.write(artist)
